71d4f318736dc1916a8a0704433a903e475a3f74
TheEmeraldStarr <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2020-04-12 10:46:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/weeb/',
+    prefix: '/blog/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
